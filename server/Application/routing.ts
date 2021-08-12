e4f5517dc0d5787d790b802fae285ece7f1c1a23
Max Smith <maxcasmith@gmail.com>
2021-08-12 17:54:41 +0100

    Fix app loading issue

--- server/Application/routing.ts
+++ server/Application/routing.ts
@@ -5,13 +5,12 @@ async function runEndpoint(req, res)
 {
     const route = routes.find(r => r.route == req.url && r.method == req.method);
 
-    const [controller, func] = route.call.split('@');
-    const ref = require(`../Application/Controllers/${controller}.js`);
-    const container = new ref[controller]();
-
-    route
-        ? res.send(await container[func](req))
-        : res.sendFile(path.join(__dirname, '../../client/index.html'));
+    if (route) {
+        const [controller, func] = route.call.split('@');
+        const ref = require(`../Application/Controllers/${controller}.js`);
+        const container = new ref[controller]();
+        res.send(await container[func](req))
+    } else res.sendFile(path.join(__dirname, '../../client/index.html'));
 }
 
 module.exports = function (router, jsonParser)
