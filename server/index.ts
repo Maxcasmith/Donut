bfe1a3bd8517b878366dc8234456f0fa18999c38
Max Smith <maxcasmith@gmail.com>
2021-08-12 18:49:39 +0100

    More reliable client checking

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,14 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import * as http from "http";
+import * as path from "path";
 import { Server } from "socket.io";
 
 const app = express();
 const jsonParser = bodyParser.json();
 
 app.use(cors());
-app.use(express.static('../client'));
+app.use(express.static(path.join(__dirname, '../client')));
 
 const server = http.createServer(app);
 const io = new Server(server);
