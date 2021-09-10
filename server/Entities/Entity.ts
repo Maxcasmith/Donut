82e4b03d4b542e1918700941c51acf23dac5c937
Max Smith <maxcasmith@gmail.com>
2021-09-10 19:12:52 +0100

    Clear Query object in Entities layer and rename setTableName to table

--- server/Entities/Entity.ts
+++ server/Entities/Entity.ts
@@ -11,33 +11,33 @@ export class Entity
 
     async create(): Promise<any>
     {
-        const data = await (new Query()).setTableName(this.table).insert(this);
+        const data = await (new Query()).table(this.table).insert(this);
         this[`${this.table}_id`] = data.insertId;
         return this;
     }
 
     async update(): Promise<any>
     {
-        const data = await (new Query()).setTableName(this.table).update(this);
+        const data = await (new Query()).table(this.table).update(this);
         return this;
     }
 
     async delete(): Promise<any>
     {
-        const data = await (new Query()).setTableName(this.table).delete(this[`${this.table}_id`]);
+        const data = await (new Query()).table(this.table).delete(this[`${this.table}_id`]);
         return data;
     }
 
     static async find(id): Promise<any>
     {
-        const data = await (new Query()).setTableName(this.name.toLowerCase()).find(id);
+        const data = await (new Query()).table(this.name.toLowerCase()).find(id);
         if (data.length == 0) throw Error(`No ${this.name} related to id ${id}`);
         return new this(data[0]);
     }
 
     static async all(): Promise<any>
     {
-        const data = await (new Query()).setTableName(this.name.toLowerCase()).get();
+        const data = await (new Query()).table(this.name.toLowerCase()).get();
         return data.map(datum => {
             return new this(datum);
         });
--- server/Entities/Query.ts
+++ server/Entities/Query.ts
@@ -1,81 +1,32 @@
-import { MySQLQuery } from "../../database/MySQLQuery.js";
-
 export class Query
 {
-    private builder:MySQLQuery;
-
-    constructor(rawQuery:string|null = null)
+    table(tableName:string): this
     {
-        this.builder = new MySQLQuery(rawQuery);
-    }
-
-    setTableName(tableName:string): this
-    {
-        this.builder.setTableName(tableName);
         return this;
     }
-
-    where(column:string, operator:string|null = '=', value:any): this
-    {
-        this.builder.where(column, operator, value);
-        return this;
-    }
-
-    orWhere(column:string, operator:string|null = '=', value:any): this
-    {
-        this.builder.orWhere(column, operator, value);
-        return this;
-    }
-
-    join(table:string, columnOne:string, columnTwo:string): this
-    {
-        this.builder.join(table, columnOne, columnTwo);
-        return this;
-    }
-
-    limit(limit:number): this
-    {
-        this.builder.limit(limit);
-        return this;
-    }
-
+    
     async get(): Promise<any>
     {
-        return await this.builder.get();
-    }
-
-    async getOneToMany(id:number, field:string): Promise<any>
-    {
-        return await this.builder.getOneToMany(id, field);
+        return "REPLACE ME";
     }
 
     async insert(data:any): Promise<any>
     {
-        return await this.builder.insert(data);
+        return "REPLACE ME";
     }
 
     async update(data:any): Promise<any>
     {
-        return await this.builder.update(data);
+        return "REPLACE ME";
     }
 
     async find(id:number): Promise<any>
     {
-        return await this.builder.find(id);
+        return "REPLACE ME";
     }
 
     async delete(id:number): Promise<any>
     {
-        return await this.builder.delete(id);
-    }
-
-    async execRawQueryString(): Promise<any>
-    {
-        return await this.builder.execRawQueryString();
-    }
-
-    async toSql(): Promise<string>
-    {
-        return await this.builder.toSql();
+        return "REPLACE ME";
     }
 }
