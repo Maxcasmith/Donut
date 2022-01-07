cb428987daeeb9e51b1c2a42beb0d538a1319ae4
Max <maxcasmith@gmail.com>
2022-01-07 18:34:44 +0000

    Add CRUD auto generation on command

---
+++ bin/Writable.js
@@ -0,0 +1,251 @@
+const fs = require('fs');
+const path = require('path');
+
+class Writable
+{
+    constructor(name, path)
+    {
+        this.name = name;
+        this.path = path;
+        this.imports = [];
+        this.functions = [];
+        this.params = [];
+    }
+
+    setExtendor(extendor)
+    {
+        this.extendor = extendor;
+        return this;
+    }
+
+    setImplementor(implementor)
+    {
+        this.implementor = implementor;
+        return this;
+    }
+
+    setTemplate(template)
+    {
+        this.template = template;
+        return this;
+    }
+
+    addImport(item, path)
+    {
+        this.imports.push({item, path});
+        return this;
+    }
+
+    addFunction(name, body, options)
+    {
+        const f = { name, body };
+        
+        if (options) {
+            f['async'] = options['async'];
+            f['args'] = options['args'];
+            f['returns'] = options['returns'];
+        }
+        this.functions.push(f);
+        return this;
+    }
+    
+    addParam(name, type, defaultValue)
+    {
+        this.params.push({name, type, defaultValue});
+        return this;
+    }
+
+    replaceBookmark(bookmark, template)
+    {
+        this.template = this.template.replaceAll(`{{${bookmark}}}`, template);
+        return this;
+    }
+
+    inCamel(name)
+    {
+        return name.replace(/([-_][a-z])/ig, (s) => {
+            return s.toUpperCase()
+                .replace('-', '')
+                .replace('_', '');
+        });
+    }
+
+    capitalizeFirstLetter(string)
+    {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    async fileExists()
+    {
+        return fs.existsSync(path.join(__dirname, this.path));
+    }
+
+    async write()
+    {
+        this.template = `{{Imports}}
+export class {{Name}} ${this.extendor ? "extends {{Extender}}" : this.implementor ? "implements {{Implementor}}" : ""}
+{
+    {{Params}}{{Functions}}
+}
+`;
+
+        let templateImport = ``;
+        for (let i of this.imports) {
+            templateImport += `import { ${i.item} } from "${i.path}";\n`;
+        }
+        this.replaceBookmark('Imports', templateImport);
+
+        this.replaceBookmark('Name', this.name);
+
+        if (this.extendor) this.replaceBookmark('Extender', this.extendor);
+        else if (this.implementor) this.replaceBookmark('Implementor', this.implementor);
+
+        let templateParams = "";
+        for (let p of this.params) {
+            templateParams += `private ${p.name}: ${p.type}${p.defaultValue ? ` = ${p.defaultValue}` : ""};\n    `;
+        }
+        if (templateParams != "") templateParams += "\n    ";
+        this.replaceBookmark('Params', templateParams);
+
+        let templateFunctions = "";
+        for (let f of this.functions) {
+            let tempArgs = "";
+            if (f.args) {
+                for (let a of f.args) {
+                    tempArgs += `${a}, `;
+                }
+                tempArgs = tempArgs.slice(0, -2);
+            }
+            templateFunctions += `${f.async ? "async " : ""}${f.name}(${tempArgs})${f.returns ? `: ${f.returns}` : ""}\n    {\n        ${f.body}\n    }\n\n    `;
+        }
+        if (templateFunctions != "") templateFunctions = templateFunctions.slice(0, -6);
+        this.replaceBookmark("Functions", templateFunctions);
+        
+
+        await this.createDirectory();
+
+        fs.writeFile(path.join(__dirname, this.path), this.template, (err) => {
+            if (err) throw err;
+        });
+    }
+
+    async createDirectory()
+    {
+        const dir = path.dirname(this.path);
+        if (!fs.existsSync(path.join(__dirname, dir))) fs.mkdirSync(path.join(__dirname, dir));
+    }
+}
+
+module.exports.Controller = class Controller extends Writable
+{
+    constructor(name, path, functions)
+    {
+        super(name, path);
+
+        this.imports = [
+            {item: "Request", path: "../Request"},
+            {item: "bus", path: "../Services/ExecutionBus" }
+        ];
+
+        for (let f of functions) {
+            if (typeof f === "string" || f instanceof String) {
+                f = {
+                    name: f,
+                    body: `const commands = []; // <-- REPLACE ME WITH ONE COMMAND OR AN ARRAY OF COMMANDS\n        const data = await bus.execute({ commands });\n        return data;`
+                }
+            }
+
+            this.addFunction(f.name, f.body, { args: ["req:Request"], async: true, returns: "Promise<any>" });
+        }
+    }
+
+}
+
+module.exports.Command = class Command extends Writable
+{
+    constructor(name, path, dir, parameters = [])
+    {
+        super(name, path);
+
+        this.imports = [
+            {item: "Command", path: "../Command"}
+        ];
+
+        let template = `super("${dir}");\n        `;
+        for (let p of parameters) {
+            const value = this.inCamel(p.split(':')[0]);
+            const type = p.split(':')[1];
+            
+            this.addParam(value, type);
+
+            template += `this.${value} = ${value};\n        `;
+        }
+        if (template != "") template = template.slice(0, -9);
+
+        this.addFunction('constructor', template, {
+            args: parameters.map(p => this.inCamel(p))
+        });
+
+        for (let p of parameters) {
+            const value = this.inCamel(p.split(":")[0]);
+            const type = p.split(":")[1];
+
+            this.addFunction(`get${this.capitalizeFirstLetter(value)}`, `return this.${value};`, { returns: type });
+            this.addFunction(`set${this.capitalizeFirstLetter(value)}`, `this.${value} = ${value};\n        return this;`, { args: [`${value}:${type}`], returns: "this"});
+        }
+
+        this.setExtendor("Command");
+    }
+}
+
+module.exports.CommandHandler = class CommandHandler extends Writable
+{
+    constructor(name, path, command, functionBody = `return "REPLACE ME";`)
+    {
+        super(name, path);
+
+        this.imports = [
+            {item: "CommandHandler", path: "../CommandHandler"},
+            {item: command.name, path: command.path}
+        ];
+
+        this.setImplementor('CommandHandler');
+
+        this.addFunction('handle', functionBody, { returns: "Promise<any>", args: ["command:"+command.name], async: true });
+    }
+}
+
+module.exports.Entity = class Entity extends Writable
+{
+    constructor(name, path, table, parameters = [])
+    {
+        super(name, path);
+
+        this.imports = [
+            {item: "Entity", path: "./Entity"}
+        ];
+
+        this.setExtendor("Entity");
+
+        let constructTemplate = `super('${this.capitalizeFirstLetter(table)}');\n        `;
+        for (let p of parameters) {
+            const value = p.split(":")[0];
+            const type = p.split(":")[1];
+
+            this.addParam(this.inCamel(value), type);
+
+            constructTemplate += `this.${this.inCamel(value)} = data['${value}']`;
+        }
+
+        this.addFunction("constructor", constructTemplate, { args: ["data:object"] });
+        
+        for (let p of parameters) {
+            const value = this.inCamel(p.split(":")[0]);
+            const type = p.split(":")[1];
+
+            this.addFunction(`get${this.capitalizeFirstLetter(value)}`, `return this.${value}`, { returns: type });
+            this.addFunction(`set${this.capitalizeFirstLetter(value)}`, `this.${value} = ${value};\n        return this;`, { returns: "this", args: [`${value}:${type}`] });
+        }
+    }
+}
+
---
+++ bin/make-crud.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const path = require('path');
+const { Controller, Command, CommandHandler } = require('./Writable.js');
+
+const crudMap = yaml.load(fs.readFileSync(path.join(__dirname, '../crud-map.yaml')));
+const map = yaml.load(fs.readFileSync(path.join(__dirname, '../map.yaml')));
+
+(async () => {
+    const crudSetup = crudMap['entities'];
+    const entitiesData = map['entities'];
+    
+    for (let e of crudSetup) {
+        const entity = entitiesData[e];
+
+        const listCommand = new Command(`List${e}Command`, `../server/Boundary/Commands/${e}/List${e}Command.ts`, e, ['limit:number', 'offset:number']);
+        const createCommand = new Command(`Create${e}Command`, `../server/Boundary/Commands/${e}/Create${e}Command.ts`, e, entity.values);
+        const readCommand = new Command(`Read${e}Command`, `../server/Boundary/Commands/${e}/Read${e}Command.ts`, e, ['id:number']);
+        const updateCommand = new Command(`Update${e}Command`, `../server/Boundary/Commands/${e}/Update${e}Command.ts`, e, entity.values);
+        const deleteCommand = new Command(`Delete${e}Command`, `../server/Boundary/Commands/${e}/Delete${e}Command.ts`, e, ['id:number']);
+
+        const listCommandHandler = new CommandHandler(`List${e}CommandHandler`, `../server/Boundary/CommandHandlers/${e}/List${e}CommandHandler.ts`, {
+            name: `List${e}Command`,
+            path: `../../Commands/${e}/List${e}Command`
+        }, `const q = new Query();\n        q.setTableName('${e.toLowerCase()}');\n        return await q.limit(command.getLimit())\n            .offset(command.getOffset())\n            .get();`);
+
+        listCommandHandler.addImport('Query', '../../../Entities/Query');
+
+        const attributes = (createCommand.functions.filter(f => f.name.includes("get"))).map(f => f.name);
+
+        let itemsTemplate = "";
+        for (let a of attributes) {
+            itemsTemplate += listCommand.inCamel(a.replace("get", "")) + `: command.${a}(),\n            `;
+        }
+        if (itemsTemplate != "") itemsTemplate = itemsTemplate.slice(0, -14);
+
+        const createCommandHandler = new CommandHandler(`Create${e}CommandHandler`, `../server/Boundary/CommandHandlers/${e}/Create${e}CommandHandler.ts`, {
+            name: `Create${e}Command`,
+            path: `../../Commands/${e}/Create${e}Command`
+        }, `const data = new ${e}({
+            ${itemsTemplate}
+        });\n        return await data.create();`);
+
+        createCommandHandler.addImport(e, `../../../Entities/${e}`);
+
+        const readCommandHandler = new CommandHandler(`Read${e}CommandHandler`, `../server/Boundary/CommandHandlers/${e}/Read${e}CommandHandler.ts`, {
+            name: `Read${e}Command`,
+            path: `../../Commands/${e}/Read${e}Command`
+        }, `return await ${e}.find(command.getId())`);
+
+        readCommandHandler.addImport(e, `../../../Entities/${e}`);
+
+        let updateId = null;
+        let updates = "";
+        for (let d of (updateCommand.functions.filter(f => f.name.includes("get"))).map(f => f.name)) {
+            if (updateId == null && d.includes('Id')) updateId = d;
+            d = (d.replace("get", ""));
+            updates += `if (command.get${d}()) data.set${d}(command.get${d}());\n        `;
+        }
+        if (updates != "") updates = updates.slice(0, -8);
+
+        const updateCommandHandler = new CommandHandler(`Update${e}CommandHandler`, `../server/Boundary/CommandHandlers/${e}/Update${e}CommandHandler.ts`, {
+            name: `Update${e}Command`,
+            path: `../../Commands/${e}/Update${e}Command`
+        }, `const data = await ${e}.find(command.${updateId}());\n        ${updates}\n        return await data.update();`);
+
+        updateCommandHandler.addImport(e, `../../../Entities/${e}`);
+
+        const deleteCommandHandler = new CommandHandler(`Delete${e}CommandHandler`, `../server/Boundary/CommandHandlers/${e}/Delete${e}CommandHandler.ts`, {
+            name: `Delete${e}Command`,
+            path: `../../Commands/${e}/Delete${e}Command`
+        }, `const data = await ${e}.find(command.getId());\n        await data.delete();\n        return { SUCCESS: true, MESSAGE: "${e} successfully deleted"}`);
+
+        deleteCommandHandler.addImport(e, `../../../Entities/${e}`);
+
+        const params = entity.values.reduce((prev, current) => `${prev.split(":")[0]}, ${current.split(":")[0]}`);
+        const controller = new Controller(e + "Controller", `../server/Application/Controllers/${e}Controller.ts`, [
+            {name: "list", body: `const {limit, offset} = req.query;\n        const commands = new List${e}Command(limit, offset);\n        const data = await bus.execute({ commands });\n        return data;`},
+            {name: "create", body: `const { ${params} } = req.body;\n        const commands = new Create${e}Command(${params});\n        const data = await bus.execute({ commands });\n        return data;`},
+            {name: "read", body: `const { id } = req.body;\n        const commands = new Read${e}Command(id);\n        const data = await bus.execute({ commands });\n        return data;`},
+            {name: "update", body: `const { ${params} } = req.body;\n        const commands = new Update${e}Command(${params});\n        const data = await bus.execute({ commands });\n        return data;`},
+            {name: "delete", body: `const { id } = req.body;\n        const commands = new Delete${e}Command(id);\n        const data = await bus.execute({ commands });\n        return data;`}
+        ]);
+
+        controller.addImport(`List${e}Command`, `../../Boundary/Commands/${e}/List${e}Command`);
+        controller.addImport(`Create${e}Command`, `../../Boundary/Commands/${e}/Create${e}Command`);
+        controller.addImport(`Read${e}Command`, `../../Boundary/Commands/${e}/Read${e}Command`);
+        controller.addImport(`Update${e}Command`, `../../Boundary/Commands/${e}/Update${e}Command`);
+        controller.addImport(`Delete${e}Command`, `../../Boundary/Commands/${e}/Delete${e}Command`);
+
+        for (let writable of [
+            listCommand, createCommand, readCommand, updateCommand, deleteCommand,
+            listCommandHandler, createCommandHandler, readCommandHandler, updateCommandHandler, deleteCommandHandler,
+            controller
+        ]) {
+            if (!await writable.fileExists()) await writable.write();
+        }
+    }
+})();
+
